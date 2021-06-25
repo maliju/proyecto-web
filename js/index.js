04d96bfdfaf09ca61a1e7998aa361b041a7d7acc
Lina <linawharff27@gmail.com>
2021-06-24 21:30:06 -0500

    Comienzo del proyecto html css js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+window.addEventListener('load', () => {
+    var boton = document.getElementById("boton");
+
+    boton.addEventListener("click",()=> {
+        alert("Presionaste el boton");
+    });
+});
