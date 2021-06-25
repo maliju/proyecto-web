1581bef78bf904db65082f57480bc4672481930c
Lina <linawharff27@gmail.com>
2021-06-24 21:53:50 -0500

    Agregue funcion factorial

--- js/index.js
+++ js/index.js
@@ -4,6 +4,28 @@ window.addEventListener('load', () => {
     var boton = document.getElementById("boton");
 
     boton.addEventListener("click",()=> {
-        alert("Presionaste el boton");
+        let numero = document.getElementById("txtNumero").value;
+
+        if (isNaN(numero)){
+           alert("FAVOR DIGITE UN NUMERO");
+        }
+        else{
+            numero = parseInt(numero);
+            let resultado = 0;
+            resultado = factorial(numero);
+            alert("El factorial de " + numero + " es " + resultado);
+        }
+        
+        
+
     });
 });
+
+function factorial(num){
+    if(num == 1){
+        return 1
+    }
+
+    return num * factorial(num - 1);
+    
+}
